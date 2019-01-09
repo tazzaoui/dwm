a93d13763c41ba778424c8a6404b9868080216ff
Taha Azzaoui <taha@tahaazzaoui.com>
2019-01-09 00:00:25 -0500

    Start tmux with st by default

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = {"dmenu_run", "-m",      dmenumon,  "-fn",     dmenufont,
                                  "-nb",       col_gray1, "-nf",     col_gray3, "-sb",
                                  col_cyan,    "-sf",     col_gray4, NULL};
-static const char *termcmd[] = {"st", NULL};
+static const char *termcmd[] = {"st", "-e", "tmux", NULL};
 static const char *screenshotcmd[] = {"screenshot", NULL};
 static const char *upvol[] = {"/usr/bin/all-pactl", "+5%", NULL};
 static const char *downvol[] = {"/usr/bin/all-pactl", "-5%", NULL};
